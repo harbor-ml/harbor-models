32dadf1c84f9420f12899935cc88576abea51a5d
Rehan Durrani <rdurrani@berkeley.edu>
2019-04-22 05:22:24 +0000

    fix sentiment model

--- Text/classification/imdb-cnn/imdb_cnn_container.py
+++ Text/classification/imdb-cnn/imdb_cnn_container.py
@@ -2,7 +2,7 @@ import rpc
 import os
 import sys
 import json
-import keras
+import tensorflow as tf
 
 ### CONFIG ###
 IMPORT_ERROR_RETURN_CODE = 3
@@ -12,27 +12,28 @@ MODEL_NAME = 'imdb-cnn.hdf5'
 
 class TfContainer(rpc.ModelContainerBase):
 	def __init__(self):
-		self.model = keras.models.load_model(MODEL_NAME)
-		self.word_index = keras.datasets.imdb.get_word_index()
+		self.model = tf.keras.models.load_model(MODEL_NAME)
+		self.word_index = tf.keras.datasets.imdb.get_word_index()
+		self.maxlen = 400
 		return
 
 	def predict_strings(self, inputs):
+		inputs = [str(bytes, 'utf-8') for bytes in inputs]
 		vecs = []
 		for text in inputs:
-			vecs.append([1] + [word_index.get(word, 2) for word in text.split(' ')])
-		seqs = keras.preprocessing.sequence.pad_sequences(vecs, maxlen=maxlen)
-		o = sess.graph.get_tensor_by_name('activation_1/Sigmoid:0')
-		i = sess.graph.get_tensor_by_name('input_1:0')
-		preds = sess.run(o, feed_dict={i, seqs})
+			vecs.append([1] + [self.word_index.get(word, 2) for word in text.split(' ')])
+		seqs = tf.keras.preprocessing.sequence.pad_sequences(vecs, maxlen=self.maxlen)
+		preds = self.model.predict(seqs)
+		results = []
 		for i in range(len(preds)):
 			if preds[i] > 0.5:
-				preds[i] = 'Positive'
+				results.append('Positive')
 			else:
-				preds[i] = 'Negative'
-		return preds
+				results.append('Negative')
+		return results
 
 if __name__ == "__main__":
-	print("Starting Tf SSD Container")
+	print("Starting Keras Sentiment CNN Container")
 	rpc_service = rpc.RPCService()
 	try:
 		model = TfContainer()
